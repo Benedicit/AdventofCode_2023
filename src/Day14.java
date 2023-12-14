24f0256bdff459e48c878d9934babd5017230b6f
Benedikt Weis <benedikt.georg.weis@gmail.com>
2023-12-15 00:08:03 +0100

    Committed my solution for day 14 part 2.

--- src/Day14.java
+++ src/Day14.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day14 {
     public static void main(String[] args) {
-        System.out.println(day14_Part1());
+        System.out.println(day14_Part2());
     }
     public static long day14_Part1() {
         File file = new File("/Users/benedikt/Documents/Programmieren/AdventOfCode/Inputs/2023/Tag_14.txt");
@@ -112,14 +114,90 @@ public class Day14 {
 
             columns.add(sb.toString());
         }
-        List<String> temp = new ArrayList<>();
-        for(String s: columns) {
-            StringBuilder stringBuilder = getStringBuilderNorth(s);
-            temp.add(stringBuilder.toString());
+        Map<Integer,List<String>> memo = new HashMap<>();
+        int position =0;
+        for (int i = 0; i < 1e9; i++) {
+            if(i>=10000) {
+                memo.put(i, columns);
+            }
+            List<String> temp = new ArrayList<>();
+            lines = new ArrayList<>();
+            for (String s : columns) {
+                StringBuilder stringBuilder = getStringBuilderNorth(s);
+                temp.add(stringBuilder.toString());
+            }
+            columns = new ArrayList<>(temp);
+            temp = new ArrayList<>();
+
+            for (int j = 0; j < columns.get(0).length(); j++) {
+                StringBuilder sb = new StringBuilder();
+                for (String s : columns) {
+                    sb.append(s.charAt(j));
+                }
+                lines.add(sb.toString());
+            }
+
+            for (String s : lines) {
+                StringBuilder sb = getStringBuilderNorth(s);
+                temp.add(sb.toString());
+            }
+            lines = new ArrayList<>(temp);
+            temp = new ArrayList<>();
+            columns = new ArrayList<>();
+
+            for (int j = 0; j < lines.get(0).length(); j++) {
+                StringBuilder sb = new StringBuilder();
+
+                for (String s : lines) {
+                    sb.append(s.charAt(j));
+                }
+
+                columns.add(sb.toString());
+            }
+
+
+            for (String s : columns) {
+                StringBuilder stringBuilder = getStringBuilderSouth(s);
+                temp.add(stringBuilder.toString());
+            }
+
+            columns = new ArrayList<>(temp);
+            temp = new ArrayList<>();
+
+            lines = new ArrayList<>();
+            for (int j = 0; j < columns.get(0).length(); j++) {
+                StringBuilder sb = new StringBuilder();
+                for (String s : columns) {
+                    sb.append(s.charAt(j));
+                }
+                lines.add(sb.toString());
+            }
+
+            for (String s : lines) {
+                StringBuilder sb = getStringBuilderSouth(s);
+                temp.add(sb.toString());
+            }
+            lines = new ArrayList<>(temp);
+            temp = new ArrayList<>();
+            columns = new ArrayList<>();
+            for (int j = 0; j < lines.get(0).length(); j++) {
+                StringBuilder sb = new StringBuilder();
+
+                for (String s : lines) {
+                    sb.append(s.charAt(j));
+                }
+
+                columns.add(sb.toString());
+            }
+            if(memo.containsValue(columns)) {
+                break;
+            }
         }
 
+        int howOftenLoop = (int) ((1e9 -10000) % memo.size());
+        columns = memo.get(10000+howOftenLoop);
 
-        for (String s : temp) {
+        for (String s : columns) {
             for (int i = 0; i < s.length(); i++) {
                 if(s.charAt(i)=='O') {
                     result += s.length()-i;
@@ -128,6 +206,30 @@ public class Day14 {
         }
 
         return result;
+    }
+
+    private static StringBuilder getStringBuilderSouth(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        for (int i = s.length()-2; i >=0; i--) {
+
+            char current = stringBuilder.charAt(i);
+
+            if(current == 'O'){
+                char charBefore = stringBuilder.charAt(i+1);
+                int charAfter = i+1;
+
+                while(charBefore== '.') {
+                    stringBuilder.setCharAt(charAfter,'O');
+                    stringBuilder.setCharAt(charAfter-1,'.');
+                    charAfter++;
+                    if(charAfter>=stringBuilder.length()) {
+                        break;
+                    }
+                    charBefore = stringBuilder.charAt(charAfter);
+                }
+            }
+        }
+        return stringBuilder;
     }
 
 }
